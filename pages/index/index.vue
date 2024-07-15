924d4c9683487284d9274069d0ff9f9677876f23
dadayechong <dadayechong@163.com>
2024-07-15 18:21:29 +0800

    uni组件学习

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,36 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		
+		<view class="uni-padding-wrap uni-common-mt">
+			<view class="uni-title uni-common-mt">
+				Vertical Scroll
+				<text>\n纵向滚动</text>
+			</view>
+			<view>
+				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltoupper="upper"
+					@scrolltolower="lower" @scroll="scroll">
+					<view id="demo1" class="uni-bg-red">A</view>
+					<view id="demo2" class="uni-bg-green">B</view>
+					<view id="demo3" class="uni-bg-blue">C</view>
+				</scroll-view>
+			</view>
+			<view @tap="goTop" class="uni-link uni-center uni-common-mt">
+				点击这里返回顶部
+			</view>
+		</view>
+	</view>
+	<view>
+		<view class="page-body">
+			<view class="btn-area">
+				<!-- 点击按钮跳转外部网站，使用点击跳转按钮 -->
+				<view>
+    				<button @click="openExternalUrl">打开外部链接</button>
+  				</view>
+	
+				<navigator url="/pages/my/my" open-type="switchTab" hover-class="other-navigator-hover">
+					<button type="default">跳转tab页面</button>
+				</navigator>
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,42 +39,85 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello 小程序'
+				scrollTop: 0,
+				old: {
+					scrollTop: 0
+				}
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			openExternalUrl() {
+				const url = 'https://www.baidu.com'; // 替换为你想要打开的外部链接
+				uni.openURL({
+					url: url,
+					success: (res) => {
+					console.log('打开外部链接成功', res);
+					},
+					fail: (err) => {
+					console.error('打开外部链接失败', err);
+					}
+				});
+			},
+			
+			upper: function(e) {
+				console.log(e)
+			},
+			lower: function(e) {
+				console.log(e)
+			},
+			scroll: function(e) {
+				console.log(e)
+				this.old.scrollTop = e.detail.scrollTop
+			},
+			goTop: function(e) {
+				// 解决view层不同步的问题
+				this.scrollTop = this.old.scrollTop
+				this.$nextTick(function() {
+					this.scrollTop = 0
+				});
+				uni.showToast({
+					icon: "none",
+					title: "纵向滚动 scrollTop 值已被修改为 0"
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.scroll-Y {
+		height: 300rpx;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.scroll-view_H {
+		white-space: nowrap;
+		width: 100%;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
+	.scroll-view-item {
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
 		font-size: 36rpx;
-		color: #8f8f94;
 	}
+	.scroll-view-item_H {
+		display: inline-block;
+		width: 100%;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+		font-size: 36rpx;
+	}
+	.uni-bg-red {
+		background-color: red;
+		height: 200px;
+	}
+	.uni-bg-green{
+		background-color: green;
+		height: 200px;
+	}
+	.uni-bg-blue{
+		background-color: blue;
+		height: 200px;
+	}
+	
+	
 </style>
